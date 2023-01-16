387cadec02d117cb0269a8944739a0598b8d19e4
Simen Kristiansen <simenbolstad@gmail.com>
2023-01-16 12:58:11 +0100

    Add project files.

---
+++ WordRiddles/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace WordRiddles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            File.ReadAllLines("ordliste.txt");
+        }
+    }
+}
