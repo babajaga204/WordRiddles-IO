9477fc1cba57ddfa20e272acb78b0ca3dd451441
Simen Kristiansen <simenbolstad@gmail.com>
2023-01-16 13:21:12 +0100

    initial commit

--- WordRiddles/Program.cs
+++ WordRiddles/Program.cs
@@ -1,11 +1,30 @@
-﻿namespace WordRiddles
+﻿using System.IO;
+using System.Text;
+
+namespace WordRiddles
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            File.ReadAllLines("ordliste.txt");
+            string path = "C:\\Users\\simen\\OneDrive\\Dokumenter\\Get_Academy\\Modul 3\\WordRiddles\\WordRiddles\\ordliste.txt";
+
+            var lastWord = string.Empty;
+            foreach (var line in File.ReadLines(path, Encoding.UTF8))
+            {
+                var parts = line.Split("\t");
+                var word = parts[1];
+                if (word != lastWord) { Console.WriteLine(word); }
+                lastWord = word;
+            }
+        }
+
+        private static void PrintText(string[] text)
+        {
+            foreach (var line in text)
+            {
+                Console.WriteLine(line);
+            }
         }
     }
 }
